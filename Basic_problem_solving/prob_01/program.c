56f136e07fc8996049a8001d23871e86b1923e15
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-10 17:07:54 +0600

    updated directory

---
+++ Basic_problem_solving/prob_01/program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int get_number;
+    scanf("%d", &get_number);
+
+    if (get_number == 0)
+    {
+        printf("Zero");
+    }
+    else
+    {
+        printf("Non Zero");
+    }
+}
