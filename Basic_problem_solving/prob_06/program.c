b7ca8698ac3b8c26b9b720610e62959fdee9df37
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-11 23:22:01 +0600

    problem 06 solved

---
+++ Basic_problem_solving/prob_06/program.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    float number_one;
+    int number_two;
+    char opr;
+
+    scanf("%f %c %d", &number_one, &opr, &number_two);
+
+    if(opr == '+'){
+        int sum = number_one + number_two;
+        printf("%d", sum);
+    }
+    else if (opr == '-'){
+        int sub = number_one - number_two;
+        printf("%d", sub);
+    }
+    else if(opr == '*'){
+        int mul = number_one * number_two;
+        printf("%d", mul);
+    }
+    else if(opr == '/'){
+        float divid = number_one / number_two;
+        printf("%.2f", divid);
+    }
+    
+}
