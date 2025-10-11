36d90069f5c6dd64e3f29181d924be9a791e0f33
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-11 22:30:26 +0600

    3rd problem solved

---
+++ Basic_problem_solving/prob_03/program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int get_number;
+    scanf("%d", &get_number);
+
+    if (get_number > 0)
+    {
+        printf("Positive");
+    }
+    else if (get_number < 0)
+    {
+        printf("Negative");
+    }
+    else
+    {
+        printf("Zero");
+    }
+}
