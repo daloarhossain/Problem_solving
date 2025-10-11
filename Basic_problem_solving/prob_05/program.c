7667437152a1edd765cbea2c0bcc452c4b3e1c22
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-11 22:52:17 +0600

    problem 5 solved

---
+++ Basic_problem_solving/prob_05/program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int number_one;
+    int number_two;
+
+    scanf("%d %d", &number_one, &number_two);
+
+    if(number_one > number_two){
+        printf("%d", number_one);
+    }
+    else if(number_two > number_one){
+        printf("%d", number_two);
+    }
+    else{
+        printf("Equal");
+    }
+}
