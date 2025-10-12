98d4d1db06e14e52caf1b9a167a7ba68a9e347ac
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-13 01:52:46 +0600

    Problem number 13 has been fixed. There was an issue

--- Problem_solving_with_C_and_CPP/prob_13/program.c
+++ Problem_solving_with_C_and_CPP/prob_13/program.c
@@ -8,10 +8,10 @@ int main()
     // find the maximum number among three numbers
     int min_num, max_num;
 
-    if(num_one > num_two && num_one > num_three){
+    if(num_one >= num_two && num_one >= num_three){
         max_num = num_one;
     }
-    else if(num_two > num_one && num_two > num_three){
+    else if(num_two > num_one && num_two >= num_three){
         max_num = num_two;
     }
     else{
@@ -19,10 +19,10 @@ int main()
     }
 
     // find the minimum numbers among three numbers
-    if(num_one < num_two && num_one < num_three){
+    if(num_one <= num_two && num_one <= num_three){
         min_num = num_one;
     }
-    else if(num_two < num_one && num_two < num_three){
+    else if(num_two <= num_one && num_two <= num_three){
         min_num = num_two;
     }
     else{
