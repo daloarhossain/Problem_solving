6aa5829a99d6bb19cb69592e54dbf2bdff1f84ba
daloarhossain <daloarhossain.contact@gmail.com>
2025-10-17 06:55:35 +0600

    Problem number 28 has been solved, and Hackerrank has accepted

---
+++ Problem_solving_with_C_and_CPP/prob_28/Say_It.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int counter;
+    scanf("%d", &counter);
+
+    for(int i = 1; i <= counter; i++){
+        printf("%d. I Want More Assignments\n", i);
+    }
+    return 0;
+}
