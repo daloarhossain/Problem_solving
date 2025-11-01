bddb661842a174649494a9c76ad322eadcee7b02
daloarhossain <daloarhossain.contact@gmail.com>
2025-11-01 20:21:55 +0600

    feat(Hacker Rank / C): Problem number 67 - divisibility by 3 - has been solved

---
+++ Problem_solving_with_C_and_CPP/prob_67/program.c
@@ -0,0 +1,34 @@
+/**
+ 📅 Date   : 31-10-2025
+ 📕 Title  : (Developing problem solving skills 🙂)
+ 👨‍💻 Author : Daloar Hossain
+ */
+ 
+#include<stdio.h>
+
+int main(){
+  
+  int arr_len, arr[100001];
+
+//   scanf("%d", &arr_len);
+  printf("%d", 12);
+  // for(int i = 0; i < arr_len; i++){
+  //   scanf("%d", &arr[i]);
+  // }
+  
+  // // calculate the summation
+  // int sum = 0;
+  // for(int i = 0; i < arr_len; i++){
+  //   sum += arr[i];
+  // }
+
+  // // prit the output if sum is divisiable by 3;
+  // if(sum % 2 == 0){
+  //   printf("YES");
+  // }
+  // else{
+  //   printf("NO");
+  // }
+
+  return 0; 
+}
